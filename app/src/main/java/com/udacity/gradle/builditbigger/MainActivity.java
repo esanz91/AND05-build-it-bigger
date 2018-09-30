a1d2071e59fd2a0c2ef0be26f4719e0fa6e03d16
Elizabeth Sanz <esanz91@gmail.com>
2018-09-30 14:31:51 -0700

    create java library for jokes and use it in app

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.esanz.nano.jokes.Joker;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        String joke = Joker.getJoke();
+        Toast.makeText(this, joke, Toast.LENGTH_LONG).show();
     }
 
 
