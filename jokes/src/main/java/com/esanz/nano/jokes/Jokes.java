273f96cfa6b74693dc9c25320a20ee384609ac67
Elizabeth Sanz <esanz91@gmail.com>
2018-09-30 18:17:15 -0700

    add joke api to backend with joke model

--- jokes/src/main/java/com/esanz/nano/jokes/Jokes.java
+++ jokes/src/main/java/com/esanz/nano/jokes/Jokes.java
@@ -2,8 +2,8 @@ package com.esanz.nano.jokes;
 
 public class Jokes {
 
-    public static String getJoke() {
-        return "What did the ocean say to the sailboat? \n Nothing, it just waved.";
+    public static Joke getJoke() {
+        return new Joke("What did the ocean say to the sailboat? \n Nothing, it just waved.");
     }
 
 }
